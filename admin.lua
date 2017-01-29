366365bdb409c802ca652dcb7ab514ca5eda35f5
zaoqi <zaoqiff@gmail.com>
2017-01-29 18:20:56 +0800

    增加suicide，删除kill的自杀功能

--- admin.lua
+++ admin.lua
@@ -1,3 +1,17 @@
+--Copyright (c) 2016 zaoqi  
+
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Affero General Public License as published
+--by the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Affero General Public License for more details.
+
+--You should have received a copy of the GNU Affero General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
 minetest.register_chatcommand("op", {
 	params = "<name>",
 	description = "Give all privilege to player",
@@ -11,7 +25,6 @@ minetest.register_chatcommand("op", {
 		end
 		local grantprivs = minetest.registered_privileges
 		local privs = minetest.get_player_privs(grantname)
-		local privs_unknown = ""
 		local basic_privs =
 			minetest.string_to_privs(minetest.setting_get("basic_privs") or "interact,shout")
 		for priv, _ in pairs(grantprivs) do
@@ -19,13 +32,7 @@ minetest.register_chatcommand("op", {
 					not minetest.check_player_privs(name, {privs=true}) then
 				return false, "Your privileges are insufficient."
 			end
-			if not minetest.registered_privileges[priv] then
-				privs_unknown = privs_unknown .. "Unknown privilege: " .. priv .. "\n"
-			end
 			privs[priv] = true
-		end
-		if privs_unknown ~= "" then
-			return false, privs_unknown
 		end
 		minetest.set_player_privs(grantname, privs)
 		minetest.log("action", name..' granted ('..minetest.privs_to_string(grantprivs, ', ')..') privileges to '..grantname)
@@ -81,25 +88,18 @@ minetest.register_chatcommand("deop", {
 
 minetest.register_privilege("kill", {description = "Can kill the players", give_to_singleplayer = false})
 minetest.register_chatcommand("kill", {
-	description = "Kill the player or you",
+	description = "Kill the player",
 	params = "[player]",
 	privs = {kill = true},
-	func = function(name, param)
-		local target
-		local targetname
-		if param == "" then
-			target = minetest.get_player_by_name(name)
-		else
-			target = minetest.get_player_by_name(param)
-		end
+	func = function(name, targetname)
+		local target = minetest.get_player_by_name(targetname)
 		
 		if target then
-			targetname = target:get_player_name()
 			target:set_hp(0)
 			minetest.chat_send_all(targetname .. " fell out of the world")
 			minetest.chat_send_player(name, "Killed " .. targetname)
 		else
-			minetest.chat_send_player(name, minetest.colorize("#FF0000", "Player not found: " .. param))		
+			minetest.chat_send_player(name, minetest.colorize("#FF0000", "Player not found: " .. targetname))
 		end
 	end
 })
--- init.lua
+++ init.lua
@@ -1,7 +1,24 @@
+--Copyright (c) 2016 zaoqi  
+
+--This program is free software: you can redistribute it and/or modify
+--it under the terms of the GNU Affero General Public License as published
+--by the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU Affero General Public License for more details.
+
+--You should have received a copy of the GNU Affero General Public License
+--along with this program.  If not, see <http://www.gnu.org/licenses/>.
 dofile(minetest.get_modpath("mccmd").."/admin.lua")
+minetest.after(0, function()
+	dofile(minetest.get_modpath("mccmd").."/aliases.lua")
+end)
 minetest.register_chatcommand("list", {
 	description = "show who is logged on",
-	func = function(name, _)
+	func = function(name)
 		local players = ""
 		for _, player in ipairs(minetest.get_connected_players()) do
 			players = players..player:get_player_name().."\n"
@@ -12,8 +29,15 @@ minetest.register_chatcommand("list", {
 
 minetest.register_chatcommand("ping", {
    description = "Ping",
-   func = function(_, _)
+   func = function()
       return true, "Pong!"
    end,
 })
-dofile(minetest.get_modpath("mccmd").."/aliases.lua")
+
+minetest.register_chatcommand("suicide", {
+	description = "Suicide",
+	func = function(name
+		minetest.get_player_by_name(name):set_hp(0)
+		minetest.chat_send_all(name .. " fell out of the world")
+	end
+})
